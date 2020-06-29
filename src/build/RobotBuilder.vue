f392042335ee2a718f1acda6a26aebcdc8f8189b
Zach Sarkis <zacherysarkis@gmail.com>
2020-06-29 13:35:06 -0600

    Add completed routing demo

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -75,10 +75,21 @@ import CollapsibleSection from '../shared/CollapsibleSection.vue';
 
 export default {
   name: 'RobotBuilder',
+  beforeRouteLeave(to, from, next) {
+    if (this.addedToCart) {
+      next(true);
+    } else {
+      /* eslint no-alert: 0 */
+      /* eslint no-restricted-globals: 0 */
+      const response = confirm('You have not added your robot to your cart, are you sure you want to leave?');
+      next(response);
+    }
+  },
   components: { PartSelector, CollapsibleSection },
   data() {
     return {
       availableParts,
+      addedToCart: false,
       cart: [],
       selectedRobot: {
         head: {},
@@ -108,6 +119,7 @@ export default {
       + robot.torso.cost
       + robot.base.cost;
       this.cart.push({ ...robot, cost });
+      this.addedToCart = true;
     },
   },
 };
