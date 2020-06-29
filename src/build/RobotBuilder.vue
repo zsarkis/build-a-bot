3a73563ce4c35167b0e31cf17bce3209e03e444c
Zach Sarkis <zacherysarkis@gmail.com>
2020-06-29 15:35:23 -0600

    Add vuex store

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -48,23 +48,6 @@
         position="bottom"
         @partSelected="part => selectedRobot.base=part"/>
     </div>
-    <div>
-      <h1>Cart</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Robot</th>
-            <th class="cost">Cost</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(robot, index) in cart" :key="index">
-            <td>{{robot.head.title}}</td>
-            <td class="cost">{{robot.cost}}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
   </div>
 </template>
 
@@ -118,7 +101,7 @@ export default {
       + robot.leftArm.cost
       + robot.torso.cost
       + robot.base.cost;
-      this.cart.push({ ...robot, cost });
+      this.$store.commit('addRobotToCart', { ...robot, cost });
       this.addedToCart = true;
     },
   },
@@ -231,14 +214,6 @@ export default {
   width: 210px;
   padding: 3px;
   font-size: 16px;
-}
-td, th {
-  text-align: left;
-  padding: 5px;
-  padding-right: 20px;
-}
-.cost {
-  text-align: right;
 }
 .sale-border {
   border: 3px solid red;
